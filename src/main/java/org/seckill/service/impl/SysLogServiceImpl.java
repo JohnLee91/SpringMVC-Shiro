1ed74792597b0b4cf3487dc603d379d1260666f4
JohnLee <1041051399@qq.com>
2017-07-11 15:27:50 +0800

    添加日志service实现类的分页函数

--- src/main/java/org/seckill/service/impl/SysLogServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SysLogServiceImpl.java
@@ -1,5 +1,7 @@
 package org.seckill.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.seckill.dao.SysLogDao;
 import org.seckill.entity.SysLog;
 import org.seckill.service.SysLogService;
@@ -41,5 +43,29 @@ public class SysLogServiceImpl implements SysLogService {
     @Override
     public List<SysLog> findAll() {
         return sysLogDao.findAll();
+    }
+
+    @Override
+    public PageInfo<SysLog> queryByPage(String moduleName, String options, Integer pageNo, Integer pageSize){
+
+        pageNo = pageNo == null?1:pageNo;
+        pageSize = pageSize == null?10:pageSize;
+        PageHelper.startPage(pageNo, pageSize);
+        List<SysLog> list = sysLogDao.queryByPage(moduleName,options);
+        //用PageInfo对结果进行包装
+        PageInfo<SysLog> page = new PageInfo<SysLog>(list);
+        //测试PageInfo全部属性
+/*        System.out.println("getPageNum:"+page.getPageNum());
+        System.out.println("getPageSize:"+page.getPageSize());
+        System.out.println("getStartRow:"+page.getStartRow());
+        System.out.println("getEndRow:"+page.getEndRow());
+        System.out.println("getTotal:"+page.getTotal());
+        System.out.println("getPages:"+page.getPages());
+        System.out.println("getFirstPage:"+page.getFirstPage());
+        System.out.println("getLastPage:"+page.getLastPage());
+        System.out.println("isHasPreviousPage:"+page.isHasPreviousPage());
+        System.out.println("isHasNextPage:"+page.isHasNextPage());*/
+
+        return page;
     }
 }
